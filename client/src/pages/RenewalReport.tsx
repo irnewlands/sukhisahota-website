5600a25d1b0b738bfb3f6d9510ff15d3f15edba6
irnewlands <irnewlands1@gmail.com>
2026-06-19 16:48:26 -0700

    Update RenewalReport.tsx

--- client/src/pages/RenewalReport.tsx
+++ client/src/pages/RenewalReport.tsx
@@ -1,1 +1,8 @@
-
+export default function RenewalReport() {
+  return (
+    <div className="min-h-screen p-10">
+      <h1>Renewal Report</h1>
+      <p>Content coming soon.</p>
+    </div>
+  );
+}
